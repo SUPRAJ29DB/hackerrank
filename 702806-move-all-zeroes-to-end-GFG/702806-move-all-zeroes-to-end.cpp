d63c113f09f6e62a2b648efb0340f176d0568d36
SUPRAAKASH GHOSH <suprakash8520@gmail.com>
2026-07-04 21:13:35 +0530

    Added solution - LeetHub

---
+++ 702806-move-all-zeroes-to-end-GFG/702806-move-all-zeroes-to-end.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    void pushZerosToEnd(vector<int>& arr) {
+        // code here
+        int j=-1;
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            if(arr[i]==0){
+            j=i;
+            break;
+            }
+        }
+        if(j==-1) {
+            return ;
+        }
+        for(int i=j+1;i<n;i++){
+            if (arr[i]!=0){
+                swap(arr[i],arr[j]);
+                j++;
+            }
+        }
+        
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
