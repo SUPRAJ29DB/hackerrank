31ca8c468aa7fe5e36a3025174415c754d88c00e
SUPRAAKASH GHOSH <suprakash8520@gmail.com>
2026-07-06 13:23:44 +0530

    Added solution - LeetHub

---
+++ 701235-union-of-2-sorted-arrays-GFG/701235-union-of-2-sorted-arrays.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<int> findUnion(vector<int> &a, vector<int> &b) {
+
+        vector<int> ans;
+        int m = a.size();
+        int n = b.size();
+
+        int i = 0, j = 0;
+
+        while (i < m && j < n) {
+
+            if (a[i] <= b[j]) {
+
+                if (ans.empty() || ans.back() != a[i])
+                    ans.push_back(a[i]);
+
+                i++;
+            }
+            else {
+
+                if (ans.empty() || ans.back() != b[j])
+                    ans.push_back(b[j]);
+
+                j++;
+            }
+        }
+
+        while (i < m) {
+            if (ans.empty() || ans.back() != a[i])
+                ans.push_back(a[i]);
+
+            i++;
+        }
+
+        while (j < n) {
+            if (ans.empty() || ans.back() != b[j])
+                ans.push_back(b[j]);
+
+            j++;
+        }
+
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
