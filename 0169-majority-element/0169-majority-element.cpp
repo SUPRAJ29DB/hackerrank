5865f1680c4b5e264173f806e1f3df3adbafed60
SUPRAAKASH GHOSH <suprakash8520@gmail.com>
2026-07-08 23:30:38 +0530

    Time: 0 ms (100%), Space: 28 MB (89.66%) - LeetHub - Time: O, Space: O(?)

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,19 +1,36 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            int freq=0;
-            for(int j=0;j<n;j++){
-                if (nums[i]==nums[j])
-                freq++;
-            }
-            if (freq>n/2){
-                return nums[i];
-            }
+        // int n=nums.size();
+        // int freq=0, nums=0;
+        //bruteforce
+        // for(int i=0;i<n;i++){
+        //     int freq=0;
+        //     for(int j=0;j<n;j++){
+        //         if (nums[i]==nums[j])
+        //         freq++;
+        //     }
+        //     if (freq>n/2){
+        //         return nums[i];
+        //     }
+        // }
+        // return -1;
+        int count = 0;
+        int candidate = 0;
+
+        for (int x : nums) {
+            if (count == 0)
+                candidate = x;
+
+            if (candidate == x)
+                count++;
+            else
+                count--;
         }
-        return -1;
-    }
+
+        return candidate;
+        }
+    
 };
 
 // Synced seamlessly with LeetHub Pro
