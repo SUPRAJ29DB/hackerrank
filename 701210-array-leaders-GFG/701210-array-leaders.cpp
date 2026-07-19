e681539f3b73b3c88096e9936960b8c9607377e2
SUPRAAKASH GHOSH <suprakash8520@gmail.com>
2026-07-19 16:59:16 +0530

    Added solution - Time: O(n), Space: O(n)

---
+++ 701210-array-leaders-GFG/701210-array-leaders.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    vector<int> leaders(vector<int>& arr) {
+
+        int n = arr.size();
+        vector<int> ans;
+
+        int maxi = arr[n - 1];
+        ans.push_back(maxi);
+
+        // Traverse from right to left
+        for (int i = n - 2; i >= 0; i--) {
+
+            if (arr[i] >= maxi) {
+                maxi = arr[i];
+                ans.push_back(arr[i]);
+            }
+        }
+
+        // Reverse to restore original order
+        reverse(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
