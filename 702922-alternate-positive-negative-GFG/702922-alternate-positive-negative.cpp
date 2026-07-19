bd64b1321a8d38cd1c988271710c8825f59a1561
SUPRAAKASH GHOSH <suprakash8520@gmail.com>
2026-07-19 13:32:42 +0530

    Added solution - Time: O(n), Space: O(n)

--- 702922-alternate-positive-negative-GFG/702922-alternate-positive-negative.cpp
+++ 702922-alternate-positive-negative-GFG/702922-alternate-positive-negative.cpp
@@ -1,37 +1,38 @@
 class Solution {
-  public:
-    void rearrange(vector<int> &arr) {
-        int n = arr.size();
-
-        vector<int> pos, neg;
-
-        // Store positives and negatives separately
-        for (int x : arr) {
-            if (x >= 0)
-                pos.push_back(x);
-            else
-                neg.push_back(x);
-        }
-
-        int i = 0, p = 0, q = 0;
-
-        // Alternate positive and negative
-        while (p < pos.size() && q < neg.size()) {
-            arr[i++] = pos[p++];
-            arr[i++] = neg[q++];
-        }
-
-        // Remaining positives
-        while (p < pos.size()) {
-            arr[i++] = pos[p++];
-        }
-
-        // Remaining negatives
-        while (q < neg.size()) {
-            arr[i++] = neg[q++];
-        }
-    }
+	public:
+	void rearrange(vector<int> &arr) {
+		int n = arr.size();
+		
+		vector<int> pos, neg;
+		
+		// Store positives and negatives separately
+		for (int x : arr) {
+			if (x >= 0)
+				pos.push_back(x);
+			else
+				neg.push_back(x);
+		}
+		
+		int i = 0, p = 0, q = 0;
+		
+		// Alternate positive and negative
+		while (p < pos.size() && q < neg.size()) {
+			arr[i++] = pos[p++];
+			arr[i++] = neg[q++];
+		}
+		
+		// Remaining positives
+		while (p < pos.size()) {
+			arr[i++] = pos[p++];
+		}
+		
+		// Remaining negatives
+		while (q < neg.size()) {
+			arr[i++] = neg[q++];
+		}
+	}
 };
+
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
