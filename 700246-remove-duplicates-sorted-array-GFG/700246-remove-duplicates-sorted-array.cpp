a4b2252bfb4d50e9ecaae137a71c0a4a77431f88
SUPRAAKASH GHOSH <suprakash8520@gmail.com>
2026-07-22 19:32:31 +0530

    Added solution - Time: O(n), Space: O(n)

---
+++ 700246-remove-duplicates-sorted-array-GFG/700246-remove-duplicates-sorted-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> removeDuplicates(vector<int>& arr) {
+
+        vector<int> ans;
+
+        if (arr.empty())
+            return ans;
+
+        ans.push_back(arr[0]);
+
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr[i] != arr[i - 1]) {
+                ans.push_back(arr[i]);
+            }
+        }
+
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
