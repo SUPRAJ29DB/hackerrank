261ab2aa78ddac2767602a600b5870c83f8f5177
SUPRAAKASH GHOSH <suprakash8520@gmail.com>
2026-07-22 18:53:11 +0530

    Added solution - Time: O(n log k), Space: O(k)

---
+++ 701747-kth-smallest-GFG/701747-kth-smallest.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int kthSmallest(vector<int> &arr, int k) {
+
+        priority_queue<int> pq;
+
+        for (int x : arr) {
+            pq.push(x);
+
+            if (pq.size() > k)
+                pq.pop();
+        }
+
+        return pq.top();
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
