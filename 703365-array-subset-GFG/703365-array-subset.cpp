bc916f7eea668c1c29920f4ba9cb59c84359bd51
SUPRAAKASH GHOSH <suprakash8520@gmail.com>
2026-07-23 21:47:42 +0530

    Added solution - Time: O(n + m), Space: O(n)

---
+++ 703365-array-subset-GFG/703365-array-subset.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isSubset(vector<int> &a, vector<int> &b) {
+
+        unordered_map<int, int> mp;
+
+        // Store frequency of elements in a
+        for (int x : a) {
+            mp[x]++;
+        }
+
+        // Check every element of b
+        for (int x : b) {
+
+            if (mp[x] == 0)
+                return false;
+
+            mp[x]--;
+        }
+
+        return true;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
