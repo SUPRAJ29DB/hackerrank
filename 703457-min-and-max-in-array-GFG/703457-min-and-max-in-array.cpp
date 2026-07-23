2fe3887658e31052e0d39a131d4a22f3744071d7
SUPRAAKASH GHOSH <suprakash8520@gmail.com>
2026-07-23 16:37:06 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 703457-min-and-max-in-array-GFG/703457-min-and-max-in-array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> getMinMax(vector<int> &arr) {
+
+        int mini = arr[0];
+        int maxi = arr[0];
+
+        for (int i = 1; i < arr.size(); i++) {
+
+            if (arr[i] < mini)
+                mini = arr[i];
+
+            if (arr[i] > maxi)
+                maxi = arr[i];
+        }
+
+        return {mini, maxi};
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
