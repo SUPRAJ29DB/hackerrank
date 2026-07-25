05a64c5b87b7da116b23417ef47a04b593d32974
SUPRAAKASH GHOSH <suprakash8520@gmail.com>
2026-07-26 00:54:15 +0530

    Added solution - Time: O(N log N), Space: O(N)

---
+++ 702927-alternative-sorting-GFG/702927-alternative-sorting.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> alternateSort(vector<int>& arr) {
+
+        sort(arr.begin(), arr.end());
+
+        vector<int> ans;
+
+        int left = 0;
+        int right = arr.size() - 1;
+
+        while (left < right) {
+            ans.push_back(arr[right--]); // Largest
+            ans.push_back(arr[left++]);  // Smallest
+        }
+
+        // For odd number of elements
+        if (left == right)
+            ans.push_back(arr[left]);
+
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
