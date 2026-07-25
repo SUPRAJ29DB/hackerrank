59afd3f8a56a9c5f2607e1f5b5202acaf22f7c91
SUPRAAKASH GHOSH <suprakash8520@gmail.com>
2026-07-25 23:16:00 +0530

    Added solution - Time: O(N^2), Space: O(1)

---
+++ 700143-bubble-sort-GFG/700143-bubble-sort.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    void bubbleSort(vector<int>& arr) {
+        // code here
+        for(int i=0;i<arr.size();i++){
+            for(int j=0;j<arr.size()-i-1;j++){
+                if (arr[j]>arr[j+1]){
+                    swap(arr[j],arr[j+1]);
+                }
+            }
+        }
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
