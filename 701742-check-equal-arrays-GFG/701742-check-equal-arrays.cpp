4a7cb5697c541a943de8415b1f51cedd65aa2d63
SUPRAAKASH GHOSH <suprakash8520@gmail.com>
2026-07-25 23:31:21 +0530

    Added solution - Time: O(n), Space: O(n)

---
+++ 701742-check-equal-arrays-GFG/701742-check-equal-arrays.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool checkEqual(vector<int>& a, vector<int>& b) {
+
+        if (a.size() != b.size())
+            return false;
+
+        unordered_map<int, int> mp;
+
+        // Count frequencies in a
+        for (int x : a)
+            mp[x]++;
+
+        // Remove frequencies using b
+        for (int x : b) {
+
+            if (mp[x] == 0)
+                return false;
+
+            mp[x]--;
+        }
+
+        return true;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
