3363194b3133fe519f44217d545d54d22d5150a7
SUPRAAKASH GHOSH <suprakash8520@gmail.com>
2026-07-26 16:35:32 +0530

    Added solution - Time: O(n log k), Space: O(k)

---
+++ 701358-nearly-sorted-GFG/701358-nearly-sorted.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void nearlySorted(vector<int>& arr, int k) {
+
+        priority_queue<int, vector<int>, greater<int>> pq;
+
+        int n = arr.size();
+
+        // Push first k+1 elements
+        for (int i = 0; i <= k && i < n; i++) {
+            pq.push(arr[i]);
+        }
+
+        int index = 0;
+
+        // Process remaining elements
+        for (int i = k + 1; i < n; i++) {
+            arr[index++] = pq.top();
+            pq.pop();
+
+            pq.push(arr[i]);
+        }
+
+        // Empty the heap
+        while (!pq.empty()) {
+            arr[index++] = pq.top();
+            pq.pop();
+        }
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
