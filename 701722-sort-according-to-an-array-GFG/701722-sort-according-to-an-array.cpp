9aa1c4d690ca6fc07d53fb26c5f6ccbbcea91968
SUPRAAKASH GHOSH <suprakash8520@gmail.com>
2026-07-26 16:04:45 +0530

    Added solution - Time: O(N log N + M), Space: O(N)

--- 701722-sort-according-to-an-array-GFG/701722-sort-according-to-an-array.cpp
+++ 701722-sort-according-to-an-array-GFG/701722-sort-according-to-an-array.cpp
@@ -1,42 +1,37 @@
 class Solution {
-  public:
+public:
     void relativeSort(vector<int>& a1, vector<int>& a2) {
+
         unordered_map<int, int> freq;
-        for (int num : a1) {
+
+        for (int num : a1)
             freq[num]++;
-        }
 
         int idx = 0;
-        unordered_set<int> seen;
 
-        // Place elements of a1 according to order in a2
+        // Place elements according to a2
         for (int num : a2) {
-            if (seen.find(num) == seen.end() && freq.count(num)) {
-                seen.insert(num);
-                while (freq[num] > 0) {
+            if (freq.count(num)) {
+                while (freq[num]--) {
                     a1[idx++] = num;
-                    freq[num]--;
                 }
                 freq.erase(num);
             }
         }
 
-        // Collect remaining elements not in a2
-        vector<int> remaining;
-        for (auto& entry : freq) {
-            while (entry.second > 0) {
-                remaining.push_back(entry.first);
-                entry.second--;
+        // Remaining elements
+        vector<int> rem;
+
+        for (auto &it : freq) {
+            while (it.second--) {
+                rem.push_back(it.first);
             }
         }
 
-        // Sort remaining elements in ascending order
-        sort(remaining.begin(), remaining.end());
+        sort(rem.begin(), rem.end());
 
-        // Append sorted remaining elements to a1
-        for (int num : remaining) {
+        for (int num : rem)
             a1[idx++] = num;
-        }
     }
 };
 
