916d332f8b9641503b779320932c47bfacb601ab
SUPRAAKASH GHOSH <suprakash8520@gmail.com>
2026-07-26 16:31:24 +0530

    Added solution - Time: O(N), Space: O(1)

---
+++ 703190-check-sorted-array-GFG/703190-check-sorted-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    bool isSorted(vector<int>& arr) {
+        // code here
+        for (int i=0;i<arr.size()-1;i++){
+            if(arr[i]>arr[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
