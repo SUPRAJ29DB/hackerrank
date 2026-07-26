90dc2c2956d987a0bfce2b19cf395314f90a2052
SUPRAAKASH GHOSH <suprakash8520@gmail.com>
2026-07-26 14:56:28 +0530

    Added solution - Time: O(n + m + k log k), Space: O(n + m)

---
+++ 701722-sort-according-to-an-array-GFG/701722-sort-according-to-an-array.cpp
@@ -0,0 +1,45 @@
+class Solution {
+  public:
+    void relativeSort(vector<int>& a1, vector<int>& a2) {
+        unordered_map<int, int> freq;
+        for (int num : a1) {
+            freq[num]++;
+        }
+
+        int idx = 0;
+        unordered_set<int> seen;
+
+        // Place elements of a1 according to order in a2
+        for (int num : a2) {
+            if (seen.find(num) == seen.end() && freq.count(num)) {
+                seen.insert(num);
+                while (freq[num] > 0) {
+                    a1[idx++] = num;
+                    freq[num]--;
+                }
+                freq.erase(num);
+            }
+        }
+
+        // Collect remaining elements not in a2
+        vector<int> remaining;
+        for (auto& entry : freq) {
+            while (entry.second > 0) {
+                remaining.push_back(entry.first);
+                entry.second--;
+            }
+        }
+
+        // Sort remaining elements in ascending order
+        sort(remaining.begin(), remaining.end());
+
+        // Append sorted remaining elements to a1
+        for (int num : remaining) {
+            a1[idx++] = num;
+        }
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
