ab1b2984dadf73235d9affffcc442ac540d1741f
SUPRAAKASH GHOSH <suprakash8520@gmail.com>
2026-08-05 23:25:20 +0530

    Added solution - Time: O(n), Space: O(n)

---
+++ 701202-frequencies-in-a-limited-array-GFG/701202-frequencies-in-a-limited-array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> frequencyCount(vector<int>& arr) {
+
+        int n = arr.size();
+
+        vector<int> dup(n + 1, 0);
+        vector<int> ans;
+
+        // Count frequency
+        for (int x : arr)
+        {
+            dup[x]++;
+        }
+
+        // Store frequencies
+        for (int i = 1; i <= n; i++)
+        {
+            ans.push_back(dup[i]);
+        }
+
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
