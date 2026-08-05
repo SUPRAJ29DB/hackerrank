70f91f297d8471c78ba02f522fe1ab208521b4fa
SUPRAAKASH GHOSH <suprakash8520@gmail.com>
2026-08-05 23:16:02 +0530

    Added solution - Time: O(n), Space: O(n)

---
+++ 700437-duplicates-in-a-limited-range-array-GFG/700437-duplicates-in-a-limited-range-array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& arr) {
+
+        vector<int> ans;
+        vector<int> dup(arr.size() + 1, 0);
+
+        // Count frequencies
+        for (int x : arr)
+        {
+            dup[x]++;
+        }
+
+        // Find duplicates
+        for (int i = 1; i <= arr.size(); i++)
+        {
+            if (dup[i] == 2)
+            {
+                ans.push_back(i);
+            }
+        }
+
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
