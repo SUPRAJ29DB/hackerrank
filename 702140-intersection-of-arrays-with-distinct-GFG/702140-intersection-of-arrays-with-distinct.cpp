3b3662d77909990a1f82f5aa5e8b626840d3f43e
SUPRAAKASH GHOSH <suprakash8520@gmail.com>
2026-08-06 20:27:28 +0530

    Added solution - Time: O(n + m), Space: O(n)

---
+++ 702140-intersection-of-arrays-with-distinct-GFG/702140-intersection-of-arrays-with-distinct.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    int intersectSize(vector<int>& a, vector<int>& b) {
+        unordered_set<int> st(a.begin(), a.end());
+        int count = 0;
+
+        for (int x : b) {
+            if (st.count(x)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
