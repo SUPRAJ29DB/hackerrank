fede99a581c7c441c5b01d449b02be1e06f1809a
SUPRAAKASH GHOSH <suprakash8520@gmail.com>
2026-08-13 00:15:46 +0530

    Added solution - Time: O(n), Space: O(n)

---
+++ 700373-max-subarray-sum-with-elements-less-than-or-equal-to-k-GFG/700373-max-subarray-sum-with-elements-less-than-or-equal-to-k.cpp
@@ -0,0 +1,32 @@
+class Solution {
+  public:
+    int maxSum(vector<int>& arr, int k) {
+        
+        int n = arr.size();
+        vector<int> dp(n, 0);
+        
+        int ans = 0;
+        
+        for(int i = 0; i < n; i++) {
+            
+            if(arr[i] <= k) {
+                
+                if(i == 0)
+                    dp[i] = arr[i];
+                else
+                    dp[i] = dp[i-1] + arr[i];
+                
+                ans = max(ans, dp[i]);
+            }
+            else {
+                dp[i] = 0;
+            }
+        }
+        
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
