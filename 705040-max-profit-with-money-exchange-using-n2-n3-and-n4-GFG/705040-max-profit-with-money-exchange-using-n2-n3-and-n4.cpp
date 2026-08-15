6e2f355a649d42be13d935d5b7a22f9cde5209ce
SUPRAAKASH GHOSH <suprakash8520@gmail.com>
2026-08-15 20:17:27 +0530

    Added solution - Time: O(log^2 n), Space: O(log^2 n)

---
+++ 705040-max-profit-with-money-exchange-using-n2-n3-and-n4-GFG/705040-max-profit-with-money-exchange-using-n2-n3-and-n4.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+
+    unordered_map<long long, long long> dp;
+
+    long long solve(long long n) {
+
+        if(n <= 1)
+            return n;
+
+        if(dp.find(n) != dp.end())
+            return dp[n];
+
+        long long exchange = solve(n / 2)
+                          + solve(n / 3)
+                          + solve(n / 4);
+
+        return dp[n] = max(n, exchange);
+    }
+
+    int maxExchangeMoney(int n) {
+        return solve(n);
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
