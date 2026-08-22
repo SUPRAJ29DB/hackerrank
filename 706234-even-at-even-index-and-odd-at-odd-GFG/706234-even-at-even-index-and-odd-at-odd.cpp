477f35322ca2301a22c1a6e60540b7ab1349d54c
SUPRAAKASH GHOSH <suprakash8520@gmail.com>
2026-08-22 21:17:01 +0530

    Added solution: Time  : O(n)                           Space : O(1)

---
+++ 706234-even-at-even-index-and-odd-at-odd-GFG/706234-even-at-even-index-and-odd-at-odd.cpp
@@ -0,0 +1,35 @@
+class Solution {
+  public:
+    vector<int> reArrange(vector<int>& arr) {
+
+        int n = arr.size();
+        int even = 0;
+        int odd = 1;
+
+        while(even < n && odd < n) {
+
+            while(even < n && arr[even]%2==0) {
+                even+=2;
+            }
+
+            while(odd<n && arr[odd]%2!=0) {
+                odd += 2;
+            }
+
+            if(even<n && odd<n) {
+                int temp =arr[even];
+                arr[even] = arr[odd];
+                arr[odd] = temp;
+
+                even += 2;
+                odd += 2;
+            }
+        }
+
+        return arr;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
