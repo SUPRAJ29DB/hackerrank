95009b80f62f762197370f3224e64e401163dc7b
SUPRAAKASH GHOSH <suprakash8520@gmail.com>
2026-08-23 22:05:38 +0530

    Added solution

---
+++ 700434-multiply-two-matrices-GFG/700434-multiply-two-matrices.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    vector<vector<int>> multiplyMatrices(vector<vector<int>> &a,
+                                         vector<vector<int>> &b) {
+
+        int n = a.size();
+
+        vector<vector<int>> result(n, vector<int>(n, 0));
+
+        for(int i = 0; i < n; i++) {          // row of a
+            for(int j = 0; j < n; j++) {      // column of b
+                for(int k = 0; k < n; k++) {  // multiplication
+                    result[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+
+        return result;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
