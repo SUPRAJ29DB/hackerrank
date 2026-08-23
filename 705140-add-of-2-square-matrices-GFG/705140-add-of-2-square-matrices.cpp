2357df4844beb63dc2bf206eb330db0d24cf08ac
SUPRAAKASH GHOSH <suprakash8520@gmail.com>
2026-08-23 22:16:38 +0530

    Added solution

---
+++ 705140-add-of-2-square-matrices-GFG/705140-add-of-2-square-matrices.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    void addMat(vector<vector<int>>& a, vector<vector<int>>& b) {
+
+        int n = a.size();
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                a[i][j] = a[i][j] + b[i][j];
+            }
+        }
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
