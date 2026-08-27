4e9a9c2093529a68fd8f905ff9458f3176ac3053
SUPRAAKASH GHOSH <suprakash8520@gmail.com>
2026-08-27 20:59:24 +0530

    Added solution - Time: O(n * target), Space: O(target)

---
+++ 704213-count-subsets-with-sum-GFG/704213-count-subsets-with-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    int perfectSum(vector<int>& arr, int target) {
+
+        int n = arr.size();
+
+        vector<int> dp(target + 1, 0);
+
+        dp[0] = 1;
+
+        for(int i = 0; i < n; i++) {
+
+            for(int j = target; j >= arr[i]; j--) {
+
+                dp[j] = dp[j] + dp[j - arr[i]];
+            }
+        }
+
+        return dp[target];
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
