97a476cce4b9c910e41e02c7d1f21e24507666ed
SUPRAAKASH GHOSH <suprakash8520@gmail.com>
2026-08-30 17:14:00 +0530

    Added solution - Time: O(n log n), Space: O(log n)

---
+++ 703903-median-of-an-array-GFG/703903-median-of-an-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    double findMedian(vector<int> &arr) {
+        int n = arr.size();
+
+        sort(arr.begin(), arr.end());
+
+        if (n % 2 != 0) {
+            return arr[n / 2];
+        }
+
+        return (arr[(n / 2) - 1] + arr[n / 2]) / 2.0;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
