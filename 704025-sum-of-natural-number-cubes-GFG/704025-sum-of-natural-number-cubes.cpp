e32648c214d50a8058ad223b23111104819b09e5
SUPRAAKASH GHOSH <suprakash8520@gmail.com>
2026-08-30 17:11:11 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 704025-sum-of-natural-number-cubes-GFG/704025-sum-of-natural-number-cubes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    int sumOfSeries(int n) {
+        // code here
+        int sum=n*(n+1)/2;
+        return  sum*sum;
+    }
+};
+// class Solution {
+//   public:
+//     int sumOfSeries(int n) {
+//         int sum = n * (n + 1) / 2;
+//         return sum * sum;
+//     }
+// };
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
