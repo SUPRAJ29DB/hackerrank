698b785ff479e64da6c0f5f0d6b7bb8c5f0b906f
SUPRAAKASH GHOSH <suprakash8520@gmail.com>
2026-08-30 17:20:32 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 704184-count-perfect-squares-GFG/704184-count-perfect-squares.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    int countSquares(int n) {
+        int count = sqrt(n);
+        if (count * count == n) {
+            return count - 1;
+        }
+        return count;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
