550cc1f41df098b31d4f405872ac725c8e098af6
SUPRAAKASH GHOSH <suprakash8520@gmail.com>
2026-09-19 19:49:34 +0530

    Force committed solution - Time: O(N), Space: O(1)

---
+++ 700125-remove-duplicates-from-linked-list-GFG/700125-remove-duplicates-from-linked-list.cpp
@@ -0,0 +1,35 @@
+/* Structure of linked list Node
+class Node {
+public:
+    int data;
+    Node *next;
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+};*/
+class Solution {
+  public:
+    Node* removeDuplicates(Node* head) {
+
+        Node* temp = head;
+
+        while(temp != NULL && temp->next != NULL)
+        {
+            if(temp->data == temp->next->data)
+            {
+                temp->next = temp->next->next;
+            }
+            else
+            {
+                temp = temp->next;
+            }
+        }
+
+        return head;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
